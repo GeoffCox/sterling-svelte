c6ed0ee393b212d4085cf2f0b12c4850bb38aa49
Geoff Cox <geoff.cox@live.com>
2022-06-13 11:26:35 -0700

    Update index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
+	import type { SvelteComponent } from 'svelte';
+
 	import { notification } from '../stores';
 	import { currentTheme } from './_components/useCurrentTheme';
 
 	import BaseLayer from '$lib/BaseLayer.svelte';
-
 	import CheckboxExamples from './_examples/CheckboxExamples.svelte';
 	import InputExamples from './_examples/InputExamples.svelte';
 	import RadioExamples from './_examples/RadioExamples.svelte';
@@ -12,12 +13,50 @@
 	import BaseLayerExamples from './_examples/BaseLayerExamples.svelte';
 	import Checkbox from '$lib/inputs/Checkbox.svelte';
 	import ListExamples from './_examples/ListExamples.svelte';
+	import List from '$lib/lists/List.svelte';
 
 	let darkMode = false;
 
 	const toggleDarkMode = () => {
 		darkMode = !darkMode;
 	};
+
+	const examples: Record<string, { name: string; component: typeof SvelteComponent }> = {
+		select: {
+			name: 'Select',
+			component: SelectExamples
+		},
+		input: {
+			name: 'Input',
+			component: InputExamples
+		},
+		list: {
+			name: 'List',
+			component: ListExamples
+		},
+		baseLayer: {
+			name: 'BaseLayer',
+			component: BaseLayerExamples
+		},
+		button: {
+			name: 'Button',
+			component: ButtonExamples
+		},
+		checkbox: {
+			name: 'Checkbox',
+			component: CheckboxExamples
+		},
+		radio: {
+			name: 'Radio',
+			component: RadioExamples
+		}
+	};
+
+	const exampleKeys: string[] = Object.keys(examples);
+
+	let selectedExampleIndex = 0;
+	$: selectedExampleKey = exampleKeys[selectedExampleIndex];
+	$: selectedExample = examples[selectedExampleKey].component;
 </script>
 
 <div class="app" use:currentTheme={{ darkMode }} class:darkMode>
@@ -28,13 +67,16 @@
 				><div slot="after">Dark Mode</div></Checkbox
 			>
 		</div>
-		<SelectExamples />
-		<InputExamples />
-		<ListExamples />
-		<BaseLayerExamples />
-		<ButtonExamples />
-		<CheckboxExamples />
-		<RadioExamples />
+		<div class="examples">
+			<div class="example-list">
+				<List items={exampleKeys} bind:selectedIndex={selectedExampleIndex} let:item let:index>
+					{examples[exampleKeys[index]].name}
+				</List>
+			</div>
+			<div class="example">
+				<svelte:component this={selectedExample} />
+			</div>
+		</div>
 	</BaseLayer>
 </div>
 
@@ -50,5 +92,14 @@
 		top: 0;
 		height: 0.9rem;
 		z-index: 1;
+	}
+
+	.examples {
+		display: grid;
+		grid-template-columns: auto 1fr;
+	}
+
+	.example-list {
+		align-self: flex-start;
 	}
 </style>
