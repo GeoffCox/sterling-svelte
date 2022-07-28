3bbf172f056f58f0b09377906de099cd992b315a
Geoff Cox <geoff.cox@live.com>
2022-07-28 16:02:31 -0700

    Update ProgressExamples.svelte

--- src/routes/_examples/ProgressExamples.svelte
+++ src/routes/_examples/ProgressExamples.svelte
@@ -1,33 +1,82 @@
 <script lang="ts">
-	import ExampleCard from '../_components/ExampleCard.svelte';
-	import ExampleSection from '../_components/ExampleSection.svelte';
-
+	import Checkbox from '$lib/inputs/Checkbox.svelte';
 	import Progress from '$lib/display/Progress.svelte';
+	import Slider from '$lib/inputs/Slider.svelte';
+
+	import Example from '../_components/Example.svelte';
+	import Input from '$lib/inputs/Input.svelte';
 
 	let value = 35;
+	let max = 100;
 	let percent: number;
+	let disabled = false;
+	let vertical = false;
+
+	// This helps fix the lost typing of forwarded events on Input
+	type FormEvent<E extends Event = Event, T extends EventTarget = HTMLElement> = E & {
+		currentTarget: EventTarget & T;
+	};
+
+	const _onMaxChange: svelteHTML.FormEventHandler<HTMLInputElement> = (e) => {
+		const target = e.target as HTMLInputElement;
+		const parsedValue = Number.parseFloat(target.value);
+		max = isNaN(parsedValue) ? 0 : parsedValue;
+	};
+
+	const onMaxChange = (e: Event): any => {
+		_onMaxChange(e as FormEvent<Event, HTMLInputElement>);
+	};
 </script>
 
-<div>
-	<ExampleSection title="Progress">
-		<ExampleCard name="default">
-			<div class="progress-container">
-				<Progress {value} bind:percent />
-				{percent}
-			</div>
-		</ExampleCard>
-		<ExampleCard name="vertical">
-			<div class="progress-container">
-				<Progress {value} vertical />
-			</div>
-		</ExampleCard>
-		<input type="range" bind:value min={0} max={100} step={1} />
-	</ExampleSection>
-</div>
+<Example name="Progress">
+	<div class="component" class:vertical slot="component">
+		<Progress {value} {max} bind:percent {vertical} />
+		<div>{value} ({percent}%)</div>
+	</div>
+	<div class="options" slot="options">
+		<div>max</div>
+		<div><Input value={max.toString()} on:change={onMaxChange} /></div>
+		<div />
+		<div>
+			<Checkbox bind:checked={disabled}>
+				<svelte:fragment slot="after">disabled</svelte:fragment>
+			</Checkbox>
+		</div>
+		<div />
+		<div>
+			<Checkbox bind:checked={vertical}>
+				<svelte:fragment slot="after">vertical</svelte:fragment>
+			</Checkbox>
+		</div>
+		<div>value</div>
+		<div class="slider">
+			<Slider bind:value min={0} {max} />
+		</div>
+	</div>
+</Example>
 
 <style>
-	.progress-container {
-		box-sizing: content-box;
-		display: block;
+	.options {
+		align-items: center;
+		display: grid;
+		grid-template-columns: auto auto;
+		grid-gap: 1rem;
+		margin-bottom: 1rem;
+		width: max-content;
+		padding: 50px;
+	}
+
+	.component {
+		box-sizing: border-box;
+		display: grid;
+		grid-template-columns: 1fr;
+		grid-template-rows: 1fr;
+		place-content: center;
+		place-items: center;
+		padding: 0;
+	}
+
+	.slider {
+		width: 250px;
 	}
 </style>
