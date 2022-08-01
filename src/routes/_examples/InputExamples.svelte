176ab279ef1fe79416af8a50dcb2ea928fe237d1
Geoff Cox <geoff.cox@live.com>
2022-08-01 14:15:18 -0700

    Updated radio for default slot

--- src/routes/_examples/InputExamples.svelte
+++ src/routes/_examples/InputExamples.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
 	import { sendNotification } from '../../stores';
 
-	import ExampleCard from '../_components/ExampleCard.svelte';
-	import ExampleSection from '../_components/ExampleSection.svelte';
-
 	import Input from '$lib/inputs/Input.svelte';
+	import Example from '../_components/Example.svelte';
+	import Checkbox from '$lib/inputs/Checkbox.svelte';
+
+	let disabled = false;
+	let placeholder = '';
 
 	const inputOnChange = (e: Event) => {
 		const target = e.currentTarget as HTMLInputElement;
@@ -17,15 +19,44 @@
 	};
 </script>
 
-<div>
-	<ExampleSection title="Input">
-		<ExampleCard name="default">
-			<Input value="Sterling" on:input={inputOnInput} on:change={inputOnChange} />
-			<Input placeholder="Sterling" />
-		</ExampleCard>
-		<ExampleCard name="disabled">
-			<Input disabled value="Sterling" />
-			<Input disabled placeholder="Sterling" />
-		</ExampleCard>
-	</ExampleSection>
-</div>
+<Example name="Input">
+	<div class="component" slot="component">
+		<Input
+			value="Sterling"
+			{disabled}
+			{placeholder}
+			on:input={inputOnInput}
+			on:change={inputOnChange}
+		/>
+	</div>
+	<div slot="options" class="options">
+		<div />
+		<div>
+			<Checkbox bind:checked={disabled}>disabled</Checkbox>
+		</div>
+		<div>Placeholder</div>
+		<div>
+			<Input bind:value={placeholder} />
+		</div>
+		<div />
+	</div>
+</Example>
+
+<style>
+	.options {
+		align-items: center;
+		display: grid;
+		grid-template-columns: auto auto;
+		grid-gap: 1rem;
+		margin-bottom: 1rem;
+		width: max-content;
+		padding: 50px;
+	}
+
+	.component {
+		box-sizing: border-box;
+		display: flex;
+		flex-direction: column;
+		padding: 0;
+	}
+</style>
