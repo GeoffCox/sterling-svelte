66efd9b639b2833f2ad306b7c3d626224b8ed8fd
Geoff Cox <geoff.cox@live.com>
2022-08-02 09:56:53 -0700

    Update ListExamples.svelte

--- src/routes/_examples/ListExamples.svelte
+++ src/routes/_examples/ListExamples.svelte
@@ -4,60 +4,65 @@
 
 	import List from '$lib/lists/List.svelte';
 	import { sendNotification } from '../../stores';
+	import Example from '../_components/Example.svelte';
+	import Checkbox from '$lib/inputs/Checkbox.svelte';
 
 	const items = Array(500)
 		.fill(1)
 		.map((x, i) => `Item ${i + 1}`);
 
-	let disabled = true;
+	let disabled = false;
+	let horizontal = false;
 </script>
 
-<div>
-	<ExampleSection title="List">
-		<ExampleCard name="default">
-			<div class="vertical">
-				<List
-					{items}
-					let:item
-					on:itemSelected={(event) => {
-						sendNotification(
-							`List item selected [${event.detail.index}] 'Item ${event.detail.item}'`
-						);
-					}}
-				/>
-			</div>
-		</ExampleCard>
-		<ExampleCard name="disabled">
-			<div class="vertical">
-				<List {items} let:item {disabled} />
-			</div>
-			<button on:click={() => (disabled = !disabled)}>toggle</button>
-		</ExampleCard>
-		<ExampleCard name="tab key for item selection">
-			<div class="vertical">
-				<List {items} let:item selectionKeys="tab" />
-			</div>
-		</ExampleCard>
-		<ExampleCard name="horizontal">
-			<div class="horizontal">
-				<List {items} let:item horizontal />
-			</div>
-		</ExampleCard>
-	</ExampleSection>
-</div>
+<Example name="Input">
+	<div class="component" class:horizontal slot="component">
+		<List
+			{items}
+			let:item
+			{disabled}
+			selectionKeys="tab"
+			{horizontal}
+			on:itemSelected={(event) => {
+				sendNotification(`List item selected [${event.detail.index}] '${event.detail.item}'`);
+			}}
+		/>
+	</div>
+	<div slot="options" class="options">
+		<div />
+		<div>
+			<Checkbox bind:checked={disabled}>disabled</Checkbox>
+		</div>
+		<div />
+		<div>
+			<Checkbox bind:checked={horizontal}>horizontal</Checkbox>
+		</div>
+	</div>
+</Example>
 
 <style>
-	.vertical {
-		height: 410px;
+	.options {
+		align-items: center;
+		display: grid;
+		grid-template-columns: auto auto;
+		grid-gap: 1rem;
+		margin-bottom: 1rem;
+		width: max-content;
+		padding: 50px;
+	}
+
+	.component {
+		box-sizing: border-box;
 		display: flex;
 		flex-direction: column;
+		padding: 0;
+		height: 410px;
 		justify-items: stretch;
 	}
 
-	.horizontal {
+	.component.horizontal {
 		width: 410px;
-		display: flex;
+		height: unset;
 		flex-direction: row;
-		justify-items: stretch;
 	}
 </style>
