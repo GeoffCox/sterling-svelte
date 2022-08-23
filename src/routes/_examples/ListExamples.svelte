50b5c171ca7d90c3275d3f0a045fa91683c69ce1
Geoff Cox <geoff.cox@live.com>
2022-08-23 12:38:11 -0700

    Update ListExamples.svelte

--- src/routes/_examples/ListExamples.svelte
+++ src/routes/_examples/ListExamples.svelte
@@ -1,53 +1,46 @@
 <script lang="ts">
-	import List from '$lib/lists/List.svelte';
-	import { sendNotification } from '../../stores';
-	import Example from '../_components/Example.svelte';
 	import Checkbox from '$lib/inputs/Checkbox.svelte';
+	import Example from '../_components/Example.svelte';
+	import List from '$lib/lists/List.svelte';
+
+	let exampleRef: any;
 
 	const items = Array(500)
 		.fill(1)
-		.map((x, i) => `Item ${i + 1}`);
+		.map((_, i) => `Item ${i + 1}`);
 
+	let selectedIndex = 0;
+	let selectedItem = items[0];
 	let disabled = false;
 	let horizontal = false;
 </script>
 
-<Example name="List">
+<Example name="List" bind:this={exampleRef}>
 	<div class="component" class:horizontal slot="component">
 		<List
+			bind:selectedIndex
+			bind:selectedItem
 			{items}
 			let:item
 			{disabled}
 			selectionKeys="tab"
 			{horizontal}
 			on:itemSelected={(event) => {
-				sendNotification(`List item selected [${event.detail.index}] '${event.detail.item}'`);
+				exampleRef.recordEvent(`List item selected [${event.detail.index}] '${event.detail.item}'`);
 			}}
 		/>
 	</div>
-	<div slot="options" class="options">
-		<div />
-		<div>
-			<Checkbox bind:checked={disabled}>disabled</Checkbox>
-		</div>
-		<div />
-		<div>
-			<Checkbox bind:checked={horizontal}>horizontal</Checkbox>
-		</div>
-	</div>
+	<svelte:fragment slot="options">
+		<Checkbox bind:checked={disabled}>disabled</Checkbox>
+		<Checkbox bind:checked={horizontal}>horizontal</Checkbox>
+	</svelte:fragment>
+	<svelte:fragment slot="status">
+		<div>selectedIndex: {selectedIndex}</div>
+		<div>selectedItem: {selectedItem}</div>
+	</svelte:fragment>
 </Example>
 
 <style>
-	.options {
-		align-items: center;
-		display: grid;
-		grid-template-columns: auto auto;
-		grid-gap: 1rem;
-		margin-bottom: 1rem;
-		width: max-content;
-		padding: 50px;
-	}
-
 	.component {
 		box-sizing: border-box;
 		display: flex;
