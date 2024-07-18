ef7bcaafe4ea90df63c7be17ce764d9ad144e0c4
Geoff Cox <geoff.cox@live.com>
2024-07-18 11:15:44 -0700

    Set component page to be pre-render

--- src/routes/(app)/components/[component]/+page.ts
+++ src/routes/(app)/components/[component]/+page.ts
@@ -1,6 +1,10 @@
 import { componentDocs } from '../../_shared/componentDocs/componentDocs';
 import type { PageLoad } from './$types';
 
+// since there's no dynamic data here, we can prerender
+// it so that it gets served as a static asset in production
+export const prerender = true;
+
 export const load: PageLoad = ({ params }) => {
   return {
     component: params.component,
